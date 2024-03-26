7b67ef85b930844b6bc5e8939576e2ca85a889a8
LAPTOP-FCN2RE9I\Playdata <wkdalstjr94@gmail.com>
2024-03-26 20:20:17 +0900

    [feat] 모집글 상세보기 화면 기본 구현
    디테일한 css 수정 필요

---
+++ OmokFront/src/components/Section/SeekingMemberPost/Detail/SeekingMemberPostDetailAll.vue
@@ -0,0 +1,20 @@
+<template>
+    <!-- frame-wrapper -->
+    <div class="frame-wrapper">
+        <SeekingMemberPostMain />
+    </div>
+</template>
+
+<script setup>
+import SeekingMemberPostMain from './SeekingMemberPostMain/SeekingMemberPostMain.vue';
+</script>
+
+<style scoped>
+.frame-wrapper {
+    position: relative;
+    width: 1438px;
+    height: 1724px;
+    top: 77px;
+    left: 0;
+}
+</style>
