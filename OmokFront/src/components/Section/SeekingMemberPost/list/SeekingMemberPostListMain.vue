fbdf15d546408eff9dc8e66161e6130295481dd6
jihyeongeun <hyeongeun0924@gmail.com>
2024-03-27 11:21:24 +0900

    #13 [feat] seekingmemberpostlist 화면 구현
    seekingmemberpostlist 화면 구현

---
+++ OmokFront/src/components/Section/SeekingMemberPost/list/SeekingMemberPostListMain.vue
@@ -0,0 +1,29 @@
+<template>
+    <!-- section -->
+    <div class="section">
+        <SessionHeader />
+        <SessionBody />
+        <SessionContent />
+    </div>
+</template>
+
+<script setup>
+import SessionHeader from './SessionHeader/SessionHeaderDetail.vue';
+import SessionBody from './SessionBody/SessionBody.vue';
+import SessionContent from './SessionContent/SessionContent.vue';
+</script>
+
+<style scoped>
+.section {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 1px;
+    position: relative;
+    flex: 1;
+    align-self: stretch;
+    width: 100%;
+    flex-grow: 1;
+    min-height: 700px;
+}
+</style>
---
+++ OmokFront/src/components/Section/SeekingMemberPost/list/SessionContent/SessionContent.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="SMP-content">
+        <div class="ellipse"></div>
+        <img class="image-3" src="@/assets/img/plus.svg">
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.SMP-content {
+    display: inline-flex;
+    align-items: flex-start;
+    gap: 10px;
+    position: fixed;
+    bottom: 30%;
+    right: 10%;
+}
+
+.ellipse {
+    position: relative;
+    width: 72px;
+    height: 73px;
+    background-color: #b5d4e7;
+    border-radius: 36px/36.5px;
+}
+
+.image-3 {
+    position: absolute;
+    width: 36px;
+    height: 41px;
+    top: 16px;
+    left: 18px;
+}
+</style>
