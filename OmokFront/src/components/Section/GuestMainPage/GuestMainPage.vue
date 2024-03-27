2e083e5dbb7fb8cf66ee959b3f5c60cc019ced99
orlzlL <yerincho06@gmail.com>
2024-03-27 10:30:25 +0900

    [feat] guest main page 구현
    img를 사용하여 guest main page 구현 하였습니다.

---
+++ OmokFront/src/components/Section/GuestMainPage/GuestMainPage.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="guestmain">
+        <img src="@/assets/img/guestmain.png"/>
+        <div class="textbox">구인부터<br>관리까지<br>한곳에서<br></div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    .guestmain {
+        background-color: #EBEBEB;
+    }
+
+    img {
+        width: 100%;
+        height: 100%;
+        margin-bottom: -85px;
+    }
+
+    .textbox {
+        left: 18%;
+        top: 25%;
+        position: absolute;
+        font-size: 300%;
+        line-height: 130%;
+        letter-spacing: 5px;
+    }
+
+</style>
