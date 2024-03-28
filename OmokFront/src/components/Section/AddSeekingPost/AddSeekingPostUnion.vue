c3160e077cc9bb2eb7bb2fad21a3b441a69b29a9
orlzlL <yerincho06@gmail.com>
2024-03-28 11:38:58 +0900

    [feat] AddSeekingPost 페이지 추가
    새 모집글을 작성하는 화면
    모집글 목록 페이지에서 플러스 버튼 클릭하여 이동하는 라우터 추가까지 완료
    CSS 수정 필요

---
+++ OmokFront/src/components/Section/AddSeekingPost/AddSeekingPostUnion.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="section">
+        <AddSeekingPostHeader/>
+        <AddSeekingPostDetail/>
+        <AddSeekingPostContent/>
+    </div>
+</template>
+
+<script setup>
+    import AddSeekingPostHeader from '@/components/Section/AddSeekingPost/AddSeekingPostHeader.vue';
+    import AddSeekingPostDetail from '@/components/Section/AddSeekingPost/AddSeekingPostDetail.vue';
+    import AddSeekingPostContent from '@/components/Section/AddSeekingPost/AddSeekingPostContent.vue';
+</script>
+
+<style scoped>
+.section {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 1px;
+    position: relative;
+    flex: 1;
+    align-self: stretch;
+    width: 100%;
+    min-width: 600px;
+    flex-grow: 1;
+    min-height: 700px;
+}
+</style>
