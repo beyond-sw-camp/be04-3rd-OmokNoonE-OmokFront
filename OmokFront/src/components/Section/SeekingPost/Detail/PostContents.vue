2c8402e1e464393c31ca65b6a6eeef4c84118474
CJC0512 <ehdchs2006@gmail.com>
2024-03-28 11:26:26 +0900

    [refact] 모집글 상세 페이지 동적 전환 구현

--- OmokFront/src/components/Section/SeekingPost/Detail/PostContents.vue
+++ OmokFront/src/components/Section/SeekingPost/Detail/PostContents.vue
@@ -15,30 +15,47 @@ const contentProps = defineProps({
 </script>
 
 <style scoped>
+@media screen and (max-width: 1040px) {
 .postcontents {
-    position: relative;
-    width: 866px;
-    height: 1240px;
+    margin-top: 20px !important;
 }
 
 .image-5 {
-    position: absolute;
-    width: 864px;
-    height: 488px;
+  width: 80% !important;
+  min-width: 500px;
+}
+}
+
+.postcontents {
+    display: inline-flex;
+    position: relative;
+    align-items: center;
+    flex-direction: column;
+    margin-top: 2%;
+    max-width: 1980px;
+    width: 90%;
+}
+
+.image-5 {
+    position: relative;
+    align-items: center;
+    width: 80%;
     top: 0;
     left: 0;
 }
 
 .FE-BE-UX-UI {
-    position: absolute;
-    width: 809px;
-    top: 535px;
-    left: 21px;
+    position: relative;
+    align-items: center;
+    height: fit-content;
     font-family: "Outfit", Helvetica;
     font-weight: 400;
     color: #000000;
     font-size: 20px;
+    margin-top: 40px;
     letter-spacing: 0;
+    width: 80%;
+    min-width: 500px;
     line-height: normal;
 }
 
